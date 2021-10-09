1dad5e70b9bd1e84f24eb929123056805072d594
Aleksandr Mokroborodov <mokroborodov95@gmail.com>
2021-10-09 21:05:14 +0300

    hotfixes: исправить подключение и назначение шрифтов, bugfixes

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popupForm = document.querySelector('.popup__form');
-const popupInputName = document.querySelector('.popup__input-name');
+const popupInputName = document.querySelector('.popup__input_type_name');
 const profileName = document.querySelector('.profile__name');
-const popupInputDescription = document.querySelector('.popup__input-description');
+const popupInputDescription = document.querySelector('.popup__input_type_description');
 const profileDescription = document.querySelector('.profile__description');
 
 function closePopup() {
