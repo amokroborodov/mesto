2b3b79c0caaf65f606933aa6e51f32c8ab950163
Aleksandr Mokroborodov <mokroborodov95@gmail.com>
2021-10-21 11:41:16 +0300

    feature: автоматическое добавление карточек с загрузки страницы

--- scripts/index.js
+++ scripts/index.js
@@ -36,3 +36,46 @@ popupCloseButton.addEventListener('click', closePopup);
 // popup.addEventListener('mouseup', popupClickHandler);
 
 popupForm.addEventListener('submit', submitForm);
+
+const templateElement = document.querySelector('.template-element').content;
+const elements = document.querySelector('.elements');
+const initialElements = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function createElement(item) {
+  templateElement.querySelector('.element__pic-caption').textContent = item.name;
+  templateElement.querySelector('.element__pic').src = item.link;
+  return templateElement.querySelector('.element').cloneNode(true);
+
+}
+
+function appendElement(item) {
+  const element = createElement(item);
+  elements.append(element);
+}
+
+initialElements.forEach(appendElement);
